63085561f514b1d21521cdb6fb5bfcc732741b87
Bob Du <i@bobdu.cc>
2024-10-14 22:17:01 +0800

    models
    Signed-off-by: Bob Du <i@bobdu.cc>

--- main.go
+++ main.go
@@ -9,12 +9,25 @@ import (
 )
 
 func main() {
-	//feedback, err := backend.ParseNewMail("dragonplus-dmarc-report", "q62t9ea2svdi6glcofhr1r9eqcp9ora5plkvnh81")
-	//fmt.Printf("%+v %+v\n", feedback, err)
+	feedback, err := backend.ParseNewMail("dragonplus-dmarc-report", "q62t9ea2svdi6glcofhr1r9eqcp9ora5plkvnh81")
+	fmt.Printf("%+v %+v\n", feedback, err)
 	dsn := "host=localhost user=postgres password=postgres dbname=dmarc sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	fmt.Printf("%+v\n", err)
-	err = db.AutoMigrate(&backend.AggregateReport{})
-	fmt.Printf("%+v\n", err)
-
+	result := db.Create(feedback)
+	if result.Error != nil {
+		fmt.Printf("%+v\n", result.Error)
+	}
+	for _, record := range feedback.Records {
+		db.Create(record)
+		for _, dkim := range record.AuthDKIM {
+			db.Create(dkim)
+		}
+		for _, spf := range record.AuthSPF {
+			db.Create(spf)
+		}
+		for _, reason := range record.POReason {
+			db.Create(reason)
+		}
+	}
 }
