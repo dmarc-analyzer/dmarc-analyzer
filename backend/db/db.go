a1fbc2a200e094d13833ca055bbf6984fd79e869
Bob Du <i@bobdu.cc>
2025-02-11 12:05:36 +0800

    database url use env
    Signed-off-by: Bob Du <i@bobdu.cc>

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,8 +12,11 @@ import (
 var DB *gorm.DB
 
 func init() {
-	dsn := "host=localhost user=postgres password=postgres dbname=dmarc sslmode=disable"
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL not set in environment")
+	}
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	fmt.Printf("%+v\n", err)
 }
