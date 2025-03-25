054e9f048ca40c72223b5be8477d1bf46c931713
Bob Du <i@bobdu.cc>
2025-03-25 18:29:32 +0800

    fix when ip source is nil
    Signed-off-by: Bob Du <i@bobdu.cc>

--- backend/model/dbtype.go
+++ backend/model/dbtype.go
@@ -69,26 +69,36 @@ type Inet net.IP
 
 // Value returns the IP address as a string for database storage.
 // This method implements the driver.Valuer interface for the Inet type.
+// If the IP is nil, it returns nil to handle null values in the database.
 //
 // Returns:
-//   - driver.Value: The string representation of the IP address
+//   - driver.Value: The string representation of the IP address or nil if the IP is nil
 //   - error: Any error encountered during conversion
 func (ip Inet) Value() (driver.Value, error) {
+	if len(ip) == 0 {
+		return nil, nil
+	}
 	return net.IP(ip).String(), nil
 }
 
 // Scan converts a database value (string) into an Inet type.
 // This method implements the sql.Scanner interface for the Inet type.
+// It handles nil values by setting the IP to an empty value.
 //
 // Parameters:
-//   - value: The database value to scan (expected to be a string representation of an IP address)
+//   - value: The database value to scan (expected to be a string representation of an IP address or nil)
 //
 // Returns:
 //   - error: Any error encountered during scanning
 func (ip *Inet) Scan(value interface{}) error {
+	if value == nil {
+		*ip = Inet(net.IP{})
+		return nil
+	}
+
 	s, ok := value.(string)
 	if !ok {
-		return fmt.Errorf("can' scan: %s", value)
+		return fmt.Errorf("can't scan: %v", value)
 	}
 	*ip = Inet(net.ParseIP(s))
 	return nil
